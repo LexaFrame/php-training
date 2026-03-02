c86752e40eb114129d69631e17f531d196b69c09
LexaFrame <sarah.bilger@free.fr>
2026-03-03 00:01:36 +0100

    PHP Exercises PixelBay 10 - Final

--- PixelBay/exo10.php
+++ PixelBay/exo10.php
@@ -54,21 +54,33 @@ Attendus :
     Affichage conditionnel des erreurs ou du message de succès
 
 */
+// Initialisation des variables pour éviter les bugs HTML :
+$name = "";
+$email = "";
+$subject = "";
+$message = "";
 
-
-// Vérifier si la méthode est POST : si la méthode est post : alors traitement, sinon, non.
-
+// Variable permettant de stocker le message d'erreur à afficher :
 $errorMsg = [];
+
+// Variable permettant de dire si les données saisies ont toutes été valides ou non (succès de la soumission du formulaire):
 $success = false;
+
+// Options acceptées dans le menu déroulant :
 $optionsSelect = ['question', 'reclamation', 'partenariat', 'autre'];
 
+// Vérifier si la méthode est POST : si la méthode est post : alors traitement, sinon, non.
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
+    // Récupérer la saisie utilisateur soumise par $_POST, espaces retranchés
     $name = trim($_POST['name']);
     $email = trim($_POST['email']);
     $subject = trim($_POST['subject']);
     $message = trim($_POST['message']);
 
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Nettoie des caractères interdits.
+
+    // Tests de la saisie utilisateur : la saisie correspond-elle aux contraintes imposées ?
     if (empty($name)) {
         $errorMsg[] = "Merci de saisir votre nom";
     } elseif (strlen($name) <2 || strlen($name) >50) {
@@ -81,7 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $errorMsg[] = "Veuillez saisir un format d'email valide";
     };
 
-    // SI valeur_recue n’appartient PAS à valeurs_autorisees :
+    // Si la saisie utilisateur dans le menu déroulant ne correspond pas aux valeurs prédéterminées attendues :
     if (!in_array($subject, $optionsSelect)) {
         $errorMsg[] = "Veuillez sélectionner un sujet valide dans la liste déroulante";
     };
@@ -98,44 +110,56 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 };
 
-/*
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-*/
-
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Contact - PixelBay</title>
+        <style>
+            .error {color: red;}
+            .success {color: green;}
+    </style>
 </head>
 <body>
     <h1>Contactez PixelBay</h1>
 
     <!-- Affichez les erreurs ou le message de succès -->
 
+    <!-- Est-ce qu'il y a au moins un message stocké dans $errorMsg ? : -->
+    <?php if(!empty($errorMsg)):?> 
+        <ul>
+            <!-- Pour chaque message, j'affiche une ligne -->
+            <?php foreach ($errorMsg as $errorSingleMessage):?>
+                <li class="error"><?= $errorSingleMessage ?></li>
+            <?php endforeach;?>
+        </ul>
+    <?php endif;?>
 
+    <!-- Si $success === true, alors : -->
+    <?php if ($success):?>
+    <!-- Message à afficher -->
+     <p class="success">Le formulaire a été soumis avec succès !</p>
+    <?php endif;?>
 
     <!-- Formulaire -->
-
     <form class="form" method="POST" action="">
 
     <!-- Champ Nom -->
     <label for="name">Nom</label>
+        <!-- Empêcher l'injection de script malveillant avec htmlspecialchars (nom) -->
     <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($name);?>" placeholder="Entrez votre nom" required><br>
 
     <!-- Champ E-mail -->
     <label for="email">E-mail<span class="required"> *</span></label>
+        <!-- Empêcher l'injection de script malveillant avec htmlspecialchars (email) -->
     <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($email);?>" placeholder="votre.email@example.com" required><br>
 
     <!-- Menu déroulant Sujet -->
-
     <label for="subject">Sujet<span class="required"> *</span></label>
     <select id="subject" name="subject" required>
-    <option value="selection">-- Sélectionnez un sujet --</option>
+    <option value="">-- Sélectionnez un sujet --</option>
+        <!-- Vérification que la sélection de l'utilisateur est conforme à ce qui a été stocké dans la variable concernée -->
     <option value="question" <?php if ($subject === "question") echo "selected";?>>Question</option>
     <option value="reclamation" <?php if ($subject === "reclamation") echo "selected";?>>Réclamation</option>
     <option value="partenariat" <?php if ($subject === "partenariat") echo "selected";?>>Partenariat</option>
@@ -144,8 +168,11 @@ $message = $_POST['message'];
 
     <!-- Champ message -->
     <label for="message" >Message<span class="required"> *</span></label>
+    <!-- Empêcher l'injection de script malveillant avec htmlspecialchars (message) -->
     <textarea id="message" name="message" placeholder="Entrez votre message..." rows="5" cols="30" required><?php echo htmlspecialchars($message); ?>
     </textarea>
 
+    <button type="submit">Envoyer</button>
+    </form>
 </body>
 </html>
